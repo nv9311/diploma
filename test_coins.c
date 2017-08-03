b014df076dbc36e7634d0d6d4e670db0c5208576
nv94 <lavica.flow@gmail.com>
2017-08-04 00:50:47 +0200

    sizeof(coins)

--- test_coins.c
+++ test_coins.c
@@ -6,19 +6,20 @@
 #include "coins.h"
 
 int goal=350;
-int coins[14] = {2, 1, 50, 1, 100, 2, 200, 2, 50, 2, 5, 1, 10, 2};
+int coins[] = {2, 1, 50, 1, 100, 2, 200, 2, 50, 2, 5, 1, 10, 2};
+int coinsLen = sizeof(coins) / sizeof(int);
 
 void test_coins0(){
-    result* r= coins0(coins, goal, 14);
+    result* r= coins0(coins, goal, coinsLen);
     printResult(r);
 }
 
 void test_coins1(){
-    result* r= coins1(coins, 0, 13, goal, 0);
+    result* r= coins1(coins, 0, coinsLen-1, goal, 0);
     printResult(r);
 }
 
 void test_coins2(){
-    result* r= coins2(coins, 0, 13, goal, 0);
+    result* r= coins2(coins, 0, coinsLen-1, goal, 0);
     printResult(r);
 }
