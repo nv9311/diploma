a2fa4ee72559d8596714469b5158f348b98e07e8
nv94 <lavica.flow@gmail.com>
2017-08-20 00:35:35 +0200

    activity test small change

--- test_activity_selection.c
+++ test_activity_selection.c
@@ -12,25 +12,31 @@
 int numActivities = 12;
 int i = -1;
 //activity* activities = NULL;
+//activity activities[8] = {{0, 1, 3},{1, 0, 4},{2, 1, 2},{3, 4, 6},{4, 2, 9},{5, 5, 8},{6, 3, 5},{7, 4, 5}};
+/*List of compatible Activities:
+2. duration: 1 - 2
+6. duration: 3 - 5
+5. duration: 5 - 8
+*/
 activity activities[12] = {{0,44,86}, {1,7,25}, {2,37,96} , {3,83,89} , {4,27,84} , {5,49,62} , {6,44,70} , {7,44,84} , {8,16,17} , {9,58,94} , {10,27,79} , {11,26,57}};
 
 
 
 void test_activity_selection_initialize(){
-    //to do for other examples
+//to do for other examples
 }
 void test_activity_selector(){
-    activity* activities_copy = cloneActivities(activities , numActivities);
-    resultActivities* r = activitySelector(activities_copy , numActivities);
-    if (VERBOSE) printResultActivity(r);
-    freeResultActivity(r);
-    free(activities_copy);
+activity* activities_copy = cloneActivities(activities , numActivities);
+resultActivities* r = activitySelector(activities_copy , numActivities);
+if (VERBOSE) printResultActivity(r);
+freeResultActivity(r);
+free(activities_copy);
 }
 
 void test_activity_selector_hybrid(){
-    activity* activities_copy = cloneActivities(activities , numActivities);
-    resultActivities* r = activitySelectorHybrid(activities_copy , 0 , numActivities - 1 , &i);
-    if (VERBOSE) printResultActivity(r);
-    freeResultActivity(r);
-    free(activities_copy);
+activity* activities_copy = cloneActivities(activities , numActivities);
+resultActivities* r = activitySelectorHybrid(activities_copy , 0 , numActivities - 1 , &i);
+if (VERBOSE) printResultActivity(r);
+freeResultActivity(r);
+free(activities_copy);
 }
